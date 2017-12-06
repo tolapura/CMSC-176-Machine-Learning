70ae593d57326bb996b860bf7975645ebba76cb7
Tehillah Leigh Lapura <tolapura@up.edu.ph>
2017-12-06 21:57:19 +0800

    Add files via upload

---
+++ mp-1/src/linear_regressions/Main.java
@@ -0,0 +1,81 @@
+package linear_regressions;
+
+import Jama.Matrix;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static Matrix a;
+    public static Matrix b;
+    public static Matrix t;
+    public static int m = 0;
+    public static int n = 0;
+
+    public static void load_data(String fileName) throws FileNotFoundException {
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+
+        // counting the size of the file and temporarily storing the elements into an arraylist
+        while (sc.hasNextLine()) {
+            m += 1;
+            String curr = sc.nextLine();
+            StringTokenizer st = new StringTokenizer(curr, ",");
+            n = st.countTokens() - 1;
+
+            int ctr = 0;
+            while (st.hasMoreTokens()) {
+                if (ctr < n) {
+                    arr.add(Integer.parseInt(st.nextToken()));
+                } else {
+                    arr2.add(Integer.parseInt(st.nextToken()));
+                }
+                ctr++;
+            }
+        }
+
+        // setting of the elements for matrix a
+        a = new Matrix(m, n+1);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n+1; j++) {
+                if (j == 0) {
+                    a.set(i,j, 1);
+                } else {
+                    a.set(i, j, arr.get(0));
+                    arr.remove(0);
+                }
+            }
+        }
+
+        // setting of the elements for matrix b
+        b = new Matrix(m, 1);
+        for (int j = 0; j < m; j++) {
+            b.set(j, 0, arr2.get(0));
+            arr2.remove(0);
+        }
+
+        t = new Matrix(n+1, 1);
+        Random rand = new Random();
+        for (int i = 0; i < n+1; i++) {
+            int r = rand.nextInt(1000);
+            t.set(i, 0, r);
+        }
+    }
+
+    public static int cost(Matrix x, Matrix y, Matrix theta) {
+        int ans = 0;
+        Matrix h = x.times(theta).minus(y);
+        for (int i = 0; i < h.getRowDimension(); i++) {
+            h.set(i, 0, (Math.pow(h.get(i, 0), 2)));
+            ans += h.get(i, 0);
+        }
+        return ans/(2 * m);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        load_data("HousePricingRelationship.in");
+        System.out.println(cost(a, b, t));
+    }
+}
